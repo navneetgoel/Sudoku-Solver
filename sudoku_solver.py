b2c7dabb5878980cd358316efaa68a19ab1c6ee2
navneetgoel <goel.navneet4@gmail.com>
2020-12-24 17:49:16 -0800

    Added isValid method

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -75,6 +75,21 @@ class SudokuSolver:
                 return False
         
         #Checking Column
+        for row_index in range(len(self.board)):
+            if self.board[row_index][position[1]] == value and position[1] != row_index:
+                return False
+        
+        #checking boxes
+        box_x = position[1]//3
+        box_y = position[0]//3
+
+        for i in range(box_y*3, box_y*3+3):
+            for j in range(box_x*3, box_x*3+3):
+                if self.board[i][j] == value and (i,j) != position:
+                    return False
+        
+        return True
+
 
 
 
